19d67c1676a8e482302d695fc1dd32790a64b1e0
karavaan <quint.van.zijl@ah.nl>
2021-02-12 23:31:16 +0100

    little bit of styling

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,27 @@
 import { FunctionalComponent, h } from "preact";
 import Walker from "./walker";
 import { SIZE } from "./walker/constants/size";
+import { GithubIcon } from "./github-icon/github-icon";
+import styled from "styled-components";
+
+const Content = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const Link = styled.a`
+    margin: 10px;
+`;
 
 const App: FunctionalComponent = () => {
     return (
         <div id="app">
+            <Content>
+                <h1>Random Walker</h1>
+                <Link href="https://github.com/karavaan/random-walker">
+                    <GithubIcon />
+                </Link>
+            </Content>
             <Walker size={SIZE} />
         </div>
     );
